2befaa865fcec559316a4e7418032ef7ab5433da
Siva-Here <sivahere9484@gmail.com>
2024-12-28 10:54:08 +0530

    iinital

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Spline from '@splinetool/react-spline';
+import './App.css'; // Add this to include the CSS for responsiveness
+
+function App() {
+  return (
+    <div className="spline-container">
+      <Spline scene="https://prod.spline.design/wUmPtyTB37ttOf71/scene.splinecode" />
+    </div>
+  );
+}
+
+export default App;
